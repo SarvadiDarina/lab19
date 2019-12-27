83d99cc7735a762e8408dbcb48f997aa8641ade4
SarvadiDarina <56667667+SarvadiDarina@users.noreply.github.com>
2019-12-27 21:59:20 +0200

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,12 +1,23 @@
 #Використовуючи підпрограми для додавання векторів та
 # множення вектора на число, знайти вектор
 #c=a-3*b+2*c, де a,b,c .
-a = [int(input('Введіть координати вектора a: ')) for x in range(3)]
-b = [int(input('Введіть координати вектора b: ')) for x in range(3)]
-c = [int(input('Введіть координати вектора c: ')) for x in range(3)]
-b3 = list(map(lambda coordinate: coordinate * 3, b))
-c2 = list(map(lambda coordinate: coordinate * 2, c))
-a_3b = list(map(lambda x, y: x - y, a, b3))
-res = list(map(lambda x, y: x + y, a_3b, c2))
+def enter_vector(name):
+	return [int(input('Введіть {0} координату вектора {1}: '.format(x,name))) for x in range(3)]
+
+def multiply_vector(vector, scalar):
+	return list(map(lambda coordinate: coordinate * scalar, vector))
+
+def add_vector(v1, v2):
+	return list(map(lambda x,y: x + y, v1, v2))
+
+a = enter_vector('a')
+b = enter_vector('b')
+c = enter_vector('c')
+
+_3b = multiply_vector(b, -3)
+c2 = multiply_vector(c, 2)
+s1 = add_vector(a, _3b)
+res = add_vector(s1, c2)
 print(res)
+
 
